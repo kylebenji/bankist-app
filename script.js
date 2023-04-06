1c8c754d2d40ad4ee6651db107f382b224d01bcc
kylebenji <127806283+kylebenji@users.noreply.github.com>
2023-04-05 22:07:10 -0700

    adding initial date information, number formatting

--- script.js
+++ script.js
@@ -3,30 +3,79 @@
 // Data
 const account1 = {
   owner: "Jonas Schmedtmann",
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  movements: [200, 450.59, -400, 3000, -650, -130, 70.34, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+
+  movementsDates: [
+    "2019-11-18T21:31:17.178Z",
+    "2019-12-23T07:42:02.383Z",
+    "2020-01-28T09:15:04.904Z",
+    "2020-04-01T10:17:24.185Z",
+    "2020-05-08T14:11:59.604Z",
+    "2020-05-27T17:01:17.194Z",
+    "2020-07-11T23:36:17.929Z",
+    "2020-07-12T10:51:36.790Z",
+  ],
+  currency: "EUR",
+  locale: "pt-PT", // de-DE
 };
 
 const account2 = {
   owner: "Jessica Davis",
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  movements: [5000, 3400, -150, -790.66, -3210, -1000, 8500.75, -30],
   interestRate: 1.5,
   pin: 2222,
+
+  movementsDates: [
+    "2019-11-01T13:15:33.035Z",
+    "2019-11-30T09:48:16.867Z",
+    "2019-12-25T06:04:23.907Z",
+    "2020-01-25T14:18:46.235Z",
+    "2020-02-05T16:33:06.386Z",
+    "2020-04-10T14:43:26.374Z",
+    "2020-06-25T18:49:59.371Z",
+    "2020-07-26T12:01:20.894Z",
+  ],
+  currency: "USD",
+  locale: "en-US",
 };
 
 const account3 = {
   owner: "Steven Thomas Williams",
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  movements: [200, -200.97, 340, -300, -20.01, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
+
+  movementsDates: [
+    "2019-11-18T21:31:17.178Z",
+    "2019-12-23T07:42:02.383Z",
+    "2020-01-28T09:15:04.904Z",
+    "2020-04-01T10:17:24.185Z",
+    "2020-05-08T14:11:59.604Z",
+    "2020-05-27T17:01:17.194Z",
+    "2020-07-11T23:36:17.929Z",
+    "2020-07-12T10:51:36.790Z",
+  ],
+  currency: "EUR",
+  locale: "pt-PT", // de-DE
 };
 
 const account4 = {
   owner: "Sarah Smith",
-  movements: [430, 1000, 700, 50, 90],
+  movements: [430.43, 1000, 700, 50.76, 90],
   interestRate: 1,
   pin: 4444,
+
+  movementsDates: [
+    "2019-11-01T13:15:33.035Z",
+    "2019-11-30T09:48:16.867Z",
+    "2019-12-25T06:04:23.907Z",
+    "2020-01-25T14:18:46.235Z",
+    "2020-02-05T16:33:06.386Z",
+  ],
+  currency: "USD",
+  locale: "en-US",
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -68,7 +117,7 @@ const displayMovements = function (movements, sort) {
       <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-      <div class="movements__value">$${mov}</div>
+      <div class="movements__value">$${mov.toFixed(2)}</div>
     </div>
     `;
     containerMovements.insertAdjacentHTML("afterbegin", html);
@@ -80,7 +129,7 @@ const calcPrintBalance = function (account) {
     return acc + mov;
   }, 0);
   account.balance = balance;
-  labelBalance.textContent = `$${balance}`;
+  labelBalance.textContent = `$${balance.toFixed(2)}`;
 };
 
 const updateSums = function (account) {
@@ -99,8 +148,8 @@ const updateSums = function (account) {
     .reduce(function (acc, mov) {
       return acc + mov;
     }, 0);
-  labelSumIn.textContent = `$${inSum}`;
-  labelSumOut.textContent = `$${Math.abs(outSum)}`;
+  labelSumIn.textContent = `$${inSum.toFixed(2)}`;
+  labelSumOut.textContent = `$${Math.abs(outSum).toFixed(2)}`;
 
   const interest = movements
     .filter(function (mov) {
@@ -154,7 +203,7 @@ btnLogin.addEventListener("click", function (event) {
     return acc.username === inputLoginUsername.value;
   });
   console.log(currAccount);
-  if (currAccount?.pin === Number(inputLoginPin.value)) {
+  if (currAccount?.pin === +inputLoginPin.value) {
     console.log("Correct pin");
     //clear inputs
     inputLoginPin.value = inputLoginUsername.value = "";
@@ -186,7 +235,7 @@ function isValidTransfer(amount, sendAcc, recAcc) {
 
 btnTransfer.addEventListener("click", function (e) {
   e.preventDefault();
-  const amount = Number(inputTransferAmount.value);
+  const amount = +inputTransferAmount.value;
   let recievingAcc = inputTransferTo.value;
   recievingAcc = accounts.find(function (acc) {
     return acc.username === recievingAcc;
@@ -204,7 +253,7 @@ btnTransfer.addEventListener("click", function (e) {
 //bank only grants the loan if there is a single deposit greater than 10% of the loan amount
 btnLoan.addEventListener("click", function (e) {
   e.preventDefault();
-  const loanAmount = Number(inputLoanAmount.value);
+  const loanAmount = Math.floor(+inputLoanAmount.value); //round off decimals
   if (
     loanAmount > 0 &&
     currAccount.movements.some(function (mov) {
@@ -222,7 +271,7 @@ btnClose.addEventListener("click", function (e) {
   //check if credentials are correct
   if (
     currAccount.username === inputCloseUsername.value &&
-    currAccount.pin === Number(inputClosePin.value)
+    currAccount.pin === +inputClosePin.value
   ) {
     console.log("deleting");
     const index = accounts.findIndex(function (acc) {
